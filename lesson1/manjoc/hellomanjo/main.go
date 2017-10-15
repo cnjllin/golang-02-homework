7bf5f0c5a6e2c44edc4e233b50f9268504cb3112
manjoc <xiuxia.zg@gmail.com>
2017-10-15 09:57:28 +0800

    manjo homework lesson 1 , hello pc

---
+++ lesson1/manjoc/hellomanjo/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	u := r.URL
+	m, _ := url.ParseQuery(u.RawQuery)
+	value, ok := m["user"]
+	if ok {
+		fmt.Fprintf(w, "hello %s.\n", value[0])
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
