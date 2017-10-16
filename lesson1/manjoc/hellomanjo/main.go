b2b9db6b62abf75ddc3b4b6f2c935b9e08d21ef9
manjoc <xiuxia.zg@gmail.com>
2017-10-16 21:11:38 +0800

    r.formvalue

--- lesson1/manjoc/hellomanjo/main.go
+++ lesson1/manjoc/hellomanjo/main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"net/url"
+	//"net/url"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	u := r.URL
-	m, _ := url.ParseQuery(u.RawQuery)
-	value, ok := m["user"]
-	if ok {
-		fmt.Fprintf(w, "hello %s.\n", value[0])
-	}
+	//u := r.URL
+	/*
+		m, _ := url.ParseQuery(u.RawQuery)
+		value, ok := m["user"]
+		if ok {
+			fmt.Fprintf(w, "hello %s.\n", value[0])
+		}
+	*/
+	fmt.Fprintf(w, "hello %s", r.FormValue("user"))
 }
 
 func main() {
