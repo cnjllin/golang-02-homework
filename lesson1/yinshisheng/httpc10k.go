85b6f632f7cd1ccdd8904df3661aceefe619ac85
yss <yss@linux-man.cn>
2017-10-20 18:01:58 +0800

    update httpc10k.go

--- lesson1/yinshisheng/httpc10k.go
+++ lesson1/yinshisheng/httpc10k.go
@@ -9,7 +9,7 @@ import (
 
 //为什么通过浏览器访问不到？使用telnet或curl可以请求到内容。不是http协议？
 
-func handle(conn net.Conn) {
+func handle(conn net.Conn) { //数据类型不是一般只有int,string,float等，为什么net.Conn可以作为数据类型？
 	fmt.Fprintf(conn, "%s", "<p>test c10k</p>")
 	conn.Close()
 }
