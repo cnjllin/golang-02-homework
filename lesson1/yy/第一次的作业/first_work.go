8210f065e5ddd6fbb521251568c167c66dd46374
yuanyang77 <13544163873@163.com>
2017-10-20 16:16:35 +0800

    第一次的作业

---
+++ lesson1/yy/第一次的作业/first_work.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>hello %s!</h1>", r.URL)
+}
+
+func user_handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>user %s!</h1>", r.URL)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/user/", user_handler)
+	http.ListenAndServe(":80", nil)
+}
