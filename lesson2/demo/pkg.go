528155b817a3245b5081df5e2666a935d551c96b
Wang Pengcheng <wangpengcheng@4paradigm.com>
2017-10-21 17:21:23 +0800

    add pkg import demo

--- lesson2/demo/pkg.go
+++ lesson2/demo/pkg.go
@@ -1,11 +1,21 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 	"github.com/51reboot/golang-02-homework/lesson2/demo/utils"
+	//"os"
 )
 
 func main() {
-	//fmt.Println("hello")
+	fmt.Println("hello", utils.Utils_a)
 	utils.Printhello()
+	utils.Cat()
+	//var s, sep string
+	//var i int
+	//i = 1
+	//for i := 1; i < len(os.Args); i++ {
+	//	s += sep + os.Args[i]
+	//	sep = " "
+	//}
+	//fmt.Println(s)
 }
---
+++ lesson2/demo/utils/cat.go
@@ -0,0 +1,8 @@
+package utils
+
+import "fmt"
+
+func Cat() {
+	var cat = "mimi"
+	fmt.Println("hello from Cat", cat)
+}
