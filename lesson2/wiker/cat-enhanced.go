5c3f632c09696db262cce309ceb8f53d9bd89873
wiker.zhou <linwiker@gmail.com>
2017-10-22 14:58:06 +0800

    添加关键备注信息

--- lesson2/wiker/cat-enhanced.go
+++ lesson2/wiker/cat-enhanced.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	slice := os.Args[1:]
 	for _, value := range slice {
-		if strings.HasPrefix(value, "http") {
+		if strings.HasPrefix(value, "http") {  //判断是否是网站地址，以是否是http开头为根据
 			fmt.Println(CatHttp(value))
 		} else {
 			fmt.Println(CatFile(value))
