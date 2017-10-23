8b115384d8de40fd1e8f5e24a2319f84208823ff
wiker.zhou <linwiker@gmail.com>
2017-10-23 22:01:19 +0800

    增加错误处理

--- lesson2/wiker/cat-enhanced.go
+++ lesson2/wiker/cat-enhanced.go
@@ -10,9 +10,13 @@ import (
 )
 
 func main() {
+	if len(os.Args) == 1 {
+		fmt.Println("请输入正确的文件路径或者http站点名称")
+		os.Exit(1)
+	}
 	slice := os.Args[1:]
 	for _, value := range slice {
-		if strings.HasPrefix(value, "http") {  //判断是否是网站地址，以是否是http开头为根据
+		if strings.HasPrefix(value, "http") { //判断是否是网站地址，以是否是http开头为根据
 			fmt.Println(CatHttp(value))
 		} else {
 			fmt.Println(CatFile(value))
