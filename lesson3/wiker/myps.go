60d1cc3fd9e84aa7196197801645265aeec2d9b6
wiker.zhou <linwiker@gmail.com>
2017-10-28 21:27:22 +0800

    作业模拟ps命令输出

---
+++ lesson3/wiker/myps.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "io/ioutil"
+    "strconv"
+)
+
+func main() {
+    infos, err := ioutil.ReadDir("/proc")
+    if err != nil {
+        log.Fatal(err)
+    }
+    for _, info := range infos {
+        if info.IsDir(){
+            pid, err := strconv.Atoi(info.Name())
+            if err != nil {
+                continue
+            }
+            path := "/proc/" + info.Name() + "/cmdline"
+            inputfile, err := ioutil.ReadFile(path)
+            if err != nil {
+                fmt.Println(err)
+                continue
+            }
+            fmt.Println(pid,string(inputfile))
+        }
+    }
+}
+
