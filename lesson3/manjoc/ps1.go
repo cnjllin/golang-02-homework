0e69501c43cc6c7735b8c32f52ca815e54c6263e
manjoc <xiuxia.zg@gmail.com>
2017-11-02 12:54:28 +0800

    v1

---
+++ lesson3/manjoc/ps1.go
@@ -0,0 +1,32 @@
+// 代码不简练, 逻辑不清楚, 没亮点, 
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	infos, err := ioutil.ReadDir("/proc")
+	if err != nil {
+		//log.Println(err)
+	} else {
+		for _, info := range infos {
+			if info.IsDir() {
+				f, err := os.Open("/proc/" + info.Name() + "/cmdline")
+				if err != nil {
+					//log.Println(err)
+				} else {
+					ff, err := ioutil.ReadAll(f)
+					if err != nil {
+						//log.Println(err)
+					} else if string(ff) != "" {
+						fmt.Printf("%s\n", info.Name()+"\t"+string(ff))
+					}
+				}
+			}
+		}
+	}
+
+}
