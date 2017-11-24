8a6547766f0c9c5338f8a086f660f0ad8ac98bfd
Wang Pengcheng <wangpengcheng@4paradigm.com>
2017-11-25 00:06:48 +0800

    add lesson7

---
+++ lesson7/pc/http_client.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func cleanUrls(u string, urls []string) []string {
+	return nil
+}
+
+func fetch(url string) ([]string, error) {
+	var urls []string
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(resp.Status)
+	}
+	doc, err := goquery.NewDocumentFromResponse(resp)
+	if err != nil {
+		return nil, err
+	}
+	doc.Find("img").Each(func(i int, s *goquery.Selection) {
+		link, _ := s.Attr("src")
+		fmt.Println(link)
+	})
+
+	return urls, nil
+}
+
+func main() {
+	//url := "http://daily.zhihu.com/"
+	url := os.Args[1]
+	urls, err := fetch(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, u := range urls {
+		fmt.Println(u)
+	}
+}
