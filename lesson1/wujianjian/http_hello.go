5207100aa14980a93a863085ef5c82636441c3a9
DragonWujj <abc,123qwe@$d-=>
2018-01-06 09:31:50 +0800

    c10k

--- lesson1/wujianjian/http_hello.go
+++ lesson1/wujianjian/http_hello.go
@@ -6,7 +6,9 @@ import (
 )
 
 func handler(w http.ResponseWriter,r *http.Request) {
-	fmt.Fprintf(w,"Hello,world!")
+	//fmt.Fprintf(w,"Hello,world!")
+	fmt.Fprintf(w,"<h1>hello,%s!</h1>",r.URL.Path)
+	fmt.Fprintf(w,"hello,%s!",r.RemoteAddr)
 }
 func main() {
 	http.HandleFunc("/",handler)
